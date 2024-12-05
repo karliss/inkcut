karliss/inkcut

commit 8650a43ec1c22e71029170b58375155933672045

Unify serial config for serial and qtserial.
closes #13

inkcut/device/transports/qtserialport/plugin.py
@@ -15,81 +15,50 @@ import traceback
 from atom.atom import set_default
 from atom.api import List, Instance, Enum, Bool, Int, Str
 from inkcut.core.api import Plugin, Model, log
+from inkcut.device.transports.serialport.plugin import SerialConfigBase, SerialPortInfo
 from inkcut.device.plugin import DeviceTransport
-from PyQt5.QtSerialPort import QSerialPort
-from PyQt5.QtSerialPort import QSerialPortInfo
+from qtpy.QtSerialPort import QSerialPort, QSerialPortInfo
+import serial
 
-
-
-class IdNameItem:  
-    def __init__(self, id, name):  
-        self.id :int = id  
-        self.name = name 
-
-
-class QtSerialConfig(Model):
-    device_path = Str()
-    
-    #: Available serial ports
-    ports = List()
-    #: Serial port config
-    port = Str().tag(config=True)
-
-    flowcontrols = []
-    # Available FlowControls
-    flowcontrols.append( IdNameItem(QSerialPort.NoFlowControl, 'No flow control') ) 
-    flowcontrols.append( IdNameItem(QSerialPort.HardwareControl,'Hardware flow control (RTS/CTS)') ) 
-    flowcontrols.append( IdNameItem(QSerialPort.SoftwareControl,'Software flow control (XON/XOFF)') ) 
-    flowcontrols.append( IdNameItem(QSerialPort.UnknownFlowControl,'Unknown flow control (obsolete value)') ) 
-    #: FlowControl config
-    flowcontrol=Int(0).tag(config=True)
-    
-    #: Available BaudRates
-    baudrates = Enum(110, 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400, 57600, 115200, 128000, 256000)
-    #: BaudRate config
-    baudrate = Int(9600).tag(config=True)
-    
-    parities = []
-    # Available Parities
-    parities.append( IdNameItem(QSerialPort.NoParity, 'No Parity') ) 
-    parities.append( IdNameItem(QSerialPort.EvenParity, 'Even') ) 
-    parities.append( IdNameItem(QSerialPort.OddParity, 'Odd') ) 
-    parities.append( IdNameItem(QSerialPort.SpaceParity, 'Space') ) 
-    parities.append( IdNameItem(QSerialPort.MarkParity, 'Mark') ) 
-    parities.append( IdNameItem(QSerialPort.UnknownParity, 'Unknown') ) 
-    #: Parity config
-    parity=Int(0).tag(config=True)    
-
-    list_stopbits = []
-    # Available Stopbits
-    list_stopbits.append( IdNameItem(QSerialPort.OneStop, '1 stop bit') ) 
-    list_stopbits.append( IdNameItem(QSerialPort.OneAndHalfStop, '1.5 stop bits (Windows only)') ) 
-    list_stopbits.append( IdNameItem(QSerialPort.TwoStop, '2 stop bits') ) 
-    #: Stopbits config
-    stopbits=Int(1).tag(config=True) 
-    
-    bytesize = Enum(8, 7, 6, 5).tag(config=True) 
-    
-
-    # -------------------------------------------------------------------------
-    # Defaults
-    # -------------------------------------------------------------------------
+class QtSerialConfig(SerialConfigBase):
     def _default_ports(self):
-        return self.get_serial_ports()
-    
-    def _default_port(self):
-        if self.ports:
-            return self.ports[0].portName()
-        return ""    
+        result = []
+        for port in QSerialPortInfo().availablePorts():
+            info = SerialPortInfo()
+            info.device_path = port.portName()
+            info.description = "{} {}".format(port.portName(), port.description())
+            if port.hasProductIdentifier() and port.hasVendorIdentifier():
+                info.usb_pid = port.productIdentifier()
+                info.usb_vid = port.vendorIdentifier()
+            result.append(info)
+        return result
 
-    def refresh(self):
-        self.ports = self._default_ports()
-        
-    def get_serial_ports(self):
-        info_list = QSerialPortInfo()
-        serial_list = info_list.availablePorts()
-        return serial_list    
-        
+    def map_flow_control(self):
+        if self.rtscts:
+            return QSerialPort.HardwareControl
+        elif self.xonxoff:
+            return QSerialPort.SoftwareControl
+        return QSerialPort.NoFlowControl
+
+    STOP_BIT_MAPPING = {
+        serial.STOPBITS_ONE: QSerialPort.StopBits.OneStop,
+        serial.STOPBITS_ONE_POINT_FIVE: QSerialPort.StopBits.OneAndHalfStop,
+        serial.STOPBITS_TWO: QSerialPort.StopBits.TwoStop,
+    }
+
+    def map_stop_bits(self):
+        return QtSerialConfig.STOP_BIT_MAPPING[self.stopbits]
+
+    PARITY_BIT_MAPPING = {
+        serial.PARITY_NONE: QSerialPort.Parity.NoParity,
+        serial.PARITY_EVEN: QSerialPort.Parity.EvenParity,
+        serial.PARITY_ODD: QSerialPort.Parity.OddParity,
+        serial.PARITY_MARK: QSerialPort.Parity.SpaceParity,
+        serial.PARITY_SPACE: QSerialPort.Parity.MarkParity,
+    }
+
+    def map_parity(self):
+        return QtSerialConfig.PARITY_BIT_MAPPING[self.parity]
 
 class QtSerialTransport(DeviceTransport):
 
@@ -102,17 +71,17 @@ class QtSerialTransport(DeviceTransport):
 
     #: Whether a serial connection spools depends on the device (configuration)
     always_spools = set_default(False)
-    
+
     def open_serial_port(self, config):
         try:
             serial_port = QSerialPort()
-            serial_port.setPortName(config.port)
+            serial_port.setPortName(config.device_path)
             #Setting the AllDirections flag is supported on all platforms. Windows supports only this mode.
             serial_port.setBaudRate(config.baudrate, QSerialPort.AllDirections)
-            serial_port.setParity(config.parity)
-            serial_port.setStopBits(config.stopbits)
+            serial_port.setParity(config.map_parity())
+            serial_port.setStopBits(config.map_stop_bits())
             serial_port.setDataBits(config.bytesize)
-            serial_port.setFlowControl(config.flowcontrol)
+            serial_port.setFlowControl(config.map_flow_control())
             serial_port.open(QSerialPort.ReadWrite)
             return serial_port
         except Exception as e:

inkcut/device/transports/serialport/plugin.py
@@ -23,12 +23,21 @@ from serial.tools.list_ports import comports
 
 from inkcut.device.transports.raw.plugin import RawFdTransport, RawFdProtocol
 
-
 #: Reverse key values
 SERIAL_PARITIES = {v: k for k, v in serial.PARITY_NAMES.items()}
 
 
-class SerialConfig(Model):
+class SerialPortInfo(Model):
+    device_path = Str()
+    description = Str()
+    usb_pid = Int()
+    usb_vid = Int()
+
+    def __str__(self):
+        return self.description
+
+
+class SerialConfigBase(Model):
     #: Available serial ports
     ports = List()
 
@@ -48,22 +57,34 @@ class SerialConfig(Model):
     # Defaults
     # -------------------------------------------------------------------------
     def _default_ports(self):
-        return comports()
+        return []
 
     def _default_parity(self):
         return 'None'
 
     def _default_port(self):
         if self.ports:
-            return self.ports[0].device
+            return self.ports[0].device_path
         return ""
 
     def refresh(self):
         self.ports = self._default_ports()
 
 
-class SerialTransport(RawFdTransport):
+class SerialConfig(SerialConfigBase):
+    def _default_ports(self):
+        result = []
+        for port in comports():
+            info = SerialPortInfo()
+            info.device_path = port.device
+            info.description = str(port)
+            info.usb_pid = port.pid if port.pid else 0
+            info.usb_vid = port.vid if port.vid else 0
+            result.append(info)
+        return result
 
+
+class SerialTransport(RawFdTransport):
     #: Default config
     config = Instance(SerialConfig, ()).tag(config=True)
 

inkcut/device/transports/serialport/settings.enaml
@@ -19,10 +19,12 @@ from enaml.widgets.api import Container, Form, Label, ObjectCombo, SpinBox, Chec
 
 enamldef SerialPortSettingsView(Container):
     attr model
+    attr exclusive = False
     padding = 0
 
+    alias dsrdtr
     func selected_port(port, ports):
-        matches = [p for p in ports if p.device == port]
+        matches = [p for p in ports if p.device_path == port]
         return matches[0] if matches else None
 
     Form:
@@ -40,7 +42,7 @@ enamldef SerialPortSettingsView(Container):
                 selected ::
                     port = change['value']
                     if port:
-                        model.port = port.device
+                        model.port = port.device_path
                 tool_tip = textwrap.dedent("""
                 List of serial ports detected by the system. If nothing is here you
                 must install the device driver for your machine.
@@ -81,12 +83,24 @@ enamldef SerialPortSettingsView(Container):
             CheckBox: rtscts:
                 text = QApplication.translate("serialport", "RTS/CTS")
                 checked := model.rtscts
+                checked ::
+                    if change['value'] and exclusive:
+                        model.xonxoff = False
+                        model.dsrdtr = False
                 tool_tip = QApplication.translate("serialport", "Enable hardware (RTS/CTS) flow control")
             CheckBox: dsrdtr:
                 text = QApplication.translate("serialport", "DSR/DTR")
                 checked := model.dsrdtr
+                checked ::
+                    if change['value'] and exclusive:
+                        model.xonxoff = False
+                        model.rtscts = False
                 tool_tip = QApplication.translate("serialport", "Enable hardware (DSR/DTR) flow control")
             CheckBox: xonxoff:
                 text = QApplication.translate("serialport", "XON/XOFF")
                 checked := model.xonxoff
+                checked ::
+                    if change['value'] and exclusive:
+                        model.dsrdtr = False
+                        model.rtscts = False
                 tool_tip = QApplication.translate("serialport", "Enable software flow control")

inkcut/device/transports/qtserialport/settings.enaml
@@ -15,93 +15,10 @@ from inkcut.core.utils import load_icon
 from enaml.layout.api import hbox, align, spacer
 from enaml.qt.QtWidgets import QApplication
 from enaml.widgets.api import Container, Form, Label, ObjectCombo, SpinBox, CheckBox, PushButton, ComboBox
+from inkcut.device.transports.serialport.settings import SerialPortSettingsView
 
 
-enamldef SerialPortSettingsView(Container):
-    attr model
-    padding = 0
-
-    func selected_port(port, ports):
-        matches = [p for p in ports if p.portName() == port]
-        return matches[0] if matches else None
-    
-    func selected_fc(flowcontrol, flowcontrols):
-        matches = [p for p in flowcontrols if p.id  == flowcontrol]
-        return matches[0] if matches else None
-        
-    func selected_parity(parity, parities):
-        matches = [p for p in parities if p.id  == parity]
-        return matches[0] if matches else None
-    
-    func selected_stopbits(stopbits, list_stopbits):
-        matches = [p for p in list_stopbits if p.id  == stopbits]
-        return matches[0] if matches else None
-
-    Form:
-        Label:
-            text = QApplication.translate("serialport", "Port")
-        Container:
-            padding = 0
-            constraints = [
-                hbox(cb, pb),
-                align('v_center', cb, pb)
-            ]
-            ObjectCombo: cb:
-                items << model.ports
-                to_string = lambda obj: obj.portName() + " - " + obj.description() 
-                selected << selected_port(model.port, model.ports)
-                selected ::
-                    port = change['value']
-                    if port:
-                        model.port = port.portName()
-                tool_tip = textwrap.dedent("""
-                List of serial ports detected by the system. If nothing is here you
-                must install the device driver for your machine.
-                """).strip()
-            PushButton: pb:
-                text = QApplication.translate("serialport", "Refresh")
-                icon = load_icon("arrow_refresh")
-                clicked :: model.refresh()
-        Label:
-            text = QApplication.translate("serialport", "Baudrate")
-        ObjectCombo:
-            items = list(model.get_member('baudrates').items)
-            selected := model.baudrate
-        Label:
-            text = QApplication.translate("serialport", "Bytesize")
-        ObjectCombo:
-            items = list(model.get_member('bytesize').items)
-            selected := model.bytesize
-        Label:
-            text = QApplication.translate("serialport", "Parity")
-        ObjectCombo:
-            items << model.parities
-            to_string = lambda obj: obj.name
-            selected << selected_parity(model.parity, model.parities)
-            selected ::
-                    it = change['value']
-                    if it:
-                        model.parity = int(it.id)
-        Label:
-            text = QApplication.translate("serialport", "Stopbits")
-        ObjectCombo:
-            items  << model.list_stopbits
-            to_string = lambda obj: obj.name
-            selected << selected_stopbits(model.stopbits, model.list_stopbits)
-            selected ::
-                    it = change['value']
-                    if it:
-                        model.stopbits = int(it.id)
-        Label:
-            text = QApplication.translate("serialport", "Flow control")
-        ObjectCombo:
-            items  << model.flowcontrols
-            to_string = lambda obj: obj.name
-            selected << selected_fc(model.flowcontrol, model.flowcontrols)
-            selected ::
-                    fc = change['value']
-                    if fc:
-                        model.flowcontrol = int(fc.id)
-
-            
+enamldef QtSerialPortSettingsView(SerialPortSettingsView):
+    dsrdtr.visible = False
+    exclusive = True
 

inkcut/device/extensions.py
@@ -110,12 +110,18 @@ class DeviceDriver(Declarative):
         """
         return self.default_config.get('job', {}).copy()
 
-    def get_connection_config(self, id):
+    def get_connection_config(self, id, fallback=None):
         """ Pull the connection config params from the default_config 
         for the given transport id.
         """
-        cfg = self.default_config.get('connection', {}).copy()
-        return cfg.get(id, {})
+        cfg = self.default_config.get('connection', {})
+        if not cfg:
+            return {}
+        if id in cfg:
+            return cfg.get(id, {}).copy()
+        if fallback:
+            return cfg.get(fallback, {}).copy()
+        return {}
 
     def get_protocol_config(self, id):
         """ Pull the protocol config from the default_config """

inkcut/device/transports/qtserialport/manifest.enaml
@@ -11,14 +11,14 @@ from inkcut.device.extensions import DeviceTransport, DEVICE_TRANSPORT_POINT
 
 def transport_factory(driver, declaration, protocol):
     from .plugin import QtSerialTransport, QtSerialConfig
-    config = QtSerialConfig(**driver.get_connection_config('qtserial'))
+    config = QtSerialConfig(**driver.get_connection_config('qtserial', 'serial'))
     return QtSerialTransport(declaration=declaration, protocol=protocol, config=config)
 
 
 def config_view_factory():
     with enaml.imports():
-        from .settings import SerialPortSettingsView
-    return SerialPortSettingsView
+        from .settings import QtSerialPortSettingsView
+    return QtSerialPortSettingsView
 
 
 def plugin_factory():

commit 883cb7bf2b1baffd2e4472cdafd266950a42b69a

Add read support for QtSerialPort transport * fix broken qtserial transport caused by  8650a43ec1c22e71029170b58375155933672045
closes #8

inkcut/device/plugin.py
@@ -186,7 +186,7 @@ class DeviceProtocol(Model):
         if self.transport is not None:
             self.transport.write(data)
 
-    def data_received(self, data):
+    def data_received(self, data: bytes):
         """ Called when the device replies back with data. This can occur
         at any time as communication is asynchronous. The protocol should
         handle as needed.

inkcut/device/transports/qtserialport/plugin.py
@@ -35,10 +35,10 @@ class QtSerialConfig(SerialConfigBase):
 
     def map_flow_control(self):
         if self.rtscts:
-            return QSerialPort.HardwareControl
+            return QSerialPort.FlowControl.HardwareControl
         elif self.xonxoff:
-            return QSerialPort.SoftwareControl
-        return QSerialPort.NoFlowControl
+            return QSerialPort.FlowControl.SoftwareControl
+        return QSerialPort.FlowControl.NoFlowControl
 
     STOP_BIT_MAPPING = {
         serial.STOPBITS_ONE: QSerialPort.StopBits.OneStop,
@@ -60,7 +60,18 @@ class QtSerialConfig(SerialConfigBase):
     def map_parity(self):
         return QtSerialConfig.PARITY_BIT_MAPPING[self.parity]
 
+    BYTESIZE_MAPPING = {
+        5: QSerialPort.DataBits.Data5,
+        6: QSerialPort.DataBits.Data6,
+        7: QSerialPort.DataBits.Data7,
+        8: QSerialPort.DataBits.Data8,
+    }
+
+    def map_bytesize(self):
+        return QtSerialConfig.BYTESIZE_MAPPING[self.bytesize]
+
 class QtSerialTransport(DeviceTransport):
+    __slots__ = ('__weakref__',) # needed to for connection with Qt signals, at least with PyQt6
 
     #: Default config
     config = Instance(QtSerialConfig, ()).tag(config=True)
@@ -75,23 +86,32 @@ class QtSerialTransport(DeviceTransport):
     def open_serial_port(self, config):
         try:
             serial_port = QSerialPort()
-            serial_port.setPortName(config.device_path)
+            serial_port.setPortName(config.port)
             #Setting the AllDirections flag is supported on all platforms. Windows supports only this mode.
-            serial_port.setBaudRate(config.baudrate, QSerialPort.AllDirections)
+            serial_port.setBaudRate(config.baudrate, QSerialPort.Direction.AllDirections)
             serial_port.setParity(config.map_parity())
             serial_port.setStopBits(config.map_stop_bits())
-            serial_port.setDataBits(config.bytesize)
+            serial_port.setDataBits(config.map_bytesize())
             serial_port.setFlowControl(config.map_flow_control())
             serial_port.open(QSerialPort.ReadWrite)
+            foo = self.on_data_receive_ready
+            serial_port.readyRead.connect(self.on_data_receive_ready)
             return serial_port
         except Exception as e:
             log.error("{}".format(traceback.format_exc()))
-            return None    
+            return None
+
+    def on_data_receive_ready(self):
+        if not self.connection:
+            return
+        while self.connection.bytesAvailable() > 0:
+            data = self.connection.read(self.connection.bytesAvailable())
+            self.protocol.data_received(data)
+            self.last_read = data
 
     def connect(self):
         config = self.config
-        #self.device_path = config.port
-        device_path = self.device_path = config.port
+        self.device_path = config.port
         try:
             #: Save a reference
             self.protocol.transport = self

inkcut/device/transports/serialport/plugin.py
@@ -23,10 +23,6 @@ from serial.tools.list_ports import comports
 
 from inkcut.device.transports.raw.plugin import RawFdTransport, RawFdProtocol
 
-#: Reverse key values
-SERIAL_PARITIES = {v: k for k, v in serial.PARITY_NAMES.items()}
-
-
 class SerialPortInfo(Model):
     device_path = Str()
     description = Str()
@@ -46,7 +42,7 @@ class SerialConfigBase(Model):
     baudrate = Int(9600).tag(config=True)
     bytesize = Enum(serial.EIGHTBITS, serial.SEVENBITS, serial.SIXBITS,
                     serial.FIVEBITS).tag(config=True)
-    parity = Enum(*serial.PARITY_NAMES.values()).tag(config=True)
+    parity = Enum(*serial.PARITY_NAMES.keys()).tag(config=True)
     stopbits = Enum(serial.STOPBITS_ONE, serial.STOPBITS_ONE_POINT_FIVE,
                     serial.STOPBITS_TWO).tag(config=True)
     xonxoff = Bool().tag(config=True)
@@ -60,7 +56,7 @@ class SerialConfigBase(Model):
         return []
 
     def _default_parity(self):
-        return 'None'
+        return 'N'
 
     def _default_port(self):
         if self.ports:
@@ -110,7 +106,7 @@ class SerialTransport(RawFdTransport):
                 reactor,
                 baudrate=config.baudrate,
                 bytesize=config.bytesize,
-                parity=SERIAL_PARITIES[config.parity],
+                parity=config.parity,
                 stopbits=config.stopbits,
                 xonxoff=config.xonxoff,
                 rtscts=config.rtscts

inkcut/device/transports/serialport/settings.enaml
@@ -15,7 +15,7 @@ from inkcut.core.utils import load_icon
 from enaml.layout.api import hbox, align, spacer
 from enaml.qt.QtWidgets import QApplication
 from enaml.widgets.api import Container, Form, Label, ObjectCombo, SpinBox, CheckBox, PushButton
-
+import serial
 
 enamldef SerialPortSettingsView(Container):
     attr model
@@ -68,6 +68,7 @@ enamldef SerialPortSettingsView(Container):
         ObjectCombo:
             items = list(model.get_member('parity').items)
             selected := model.parity
+            to_string = lambda v: serial.PARITY_NAMES[v]
         Label:
             text = QApplication.translate("serialport", "Stopbits")
         ObjectCombo: