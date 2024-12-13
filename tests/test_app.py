2cac292b304fbac9e77c1716c808e1c69c10a352
Karliss <karlis3p70l1ij@gmail.com>
2024-12-13 02:24:19 +0200

    Fix tests.

--- tests/test_app.py
+++ tests/test_app.py
@@ -30,6 +30,11 @@ def close():
 @pytest.mark.skipif(not is_pyqtgraph_available,
                     reason='pyqtgraph is not available')
 def test_app():
+    # main app will fail to start if there already is a reactor
+    import sys
+    if "twisted.internet.reactor" in sys.modules:
+        sys.modules.pop("twisted.internet.reactor")
+
     # Must close programatically
     Timer(10, lambda: deferred_call(close)).start()
     from inkcut.app import main
