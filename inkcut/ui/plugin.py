karliss/inkcut

commit a08e087c9822eb5fa96a25ab37985d8579389243

Replace pkg_resources with importlib.metadata
pkg_resource is deprecated and removed in Pyhton 3.12.

inkcut/ui/plugin.py
@@ -11,7 +11,7 @@ Created on Jul 12, 2015
 """
 import sys
 import enaml
-import pkg_resources
+import importlib.metadata
 from datetime import datetime
 from atom.api import Atom, Int, List, Str, Instance, Bool, Enum, Dict
 from enaml.qt.q_resource_helpers import get_cached_qicon
@@ -112,8 +112,8 @@ class InkcutPlugin(Plugin):
             plugins.append(MonitorManifest)
 
             #: Load any plugins defined as extension points
-            for entry_point in pkg_resources.iter_entry_points(
-                    group='inkcut.plugin', name=None):
+            for entry_point in importlib.metadata.entry_points(
+                    group='inkcut.plugin'):
                 plugins.append(entry_point.load())
 
         #: Install all of them

commit 56696b1b6ad15e725cfc491a1cf7edec4fd7a0a1

Fix compatibility regression caused by a08e087c9822eb5fa96a25ab37985d8579389243
Metadata.entry_points was introduced in 3.8 but the filters only in 3.10.

inkcut/ui/plugin.py
@@ -112,8 +112,14 @@ class InkcutPlugin(Plugin):
             plugins.append(MonitorManifest)
 
             #: Load any plugins defined as extension points
-            for entry_point in importlib.metadata.entry_points(
-                    group='inkcut.plugin'):
+            if sys.version_info >= (3, 10, 0):
+                entry_points = importlib.metadata.entry_points(group='inkcut.plugin')
+            else:
+                entry_points = importlib.metadata.entry_points().get(
+                    'inkcut.plugin',
+                    []
+                )
+            for entry_point in entry_points:
                 plugins.append(entry_point.load())
 
         #: Install all of them